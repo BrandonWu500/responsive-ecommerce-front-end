1fb5bcd6c9d09367729d096a447bc17702d301c7
BrandonWu500 <brwu17@gmail.com>
2022-06-20 15:15:13 -0400

    Revert "initial commit"
    This reverts commit 7e57a5f550667be95842d0ab4b91b41831462749.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import Cart from './pages/Cart';
-import Home from './pages/home/Home';
-import Login from './pages/Login';
-import ProductInfo from './pages/ProductInfo';
-import ProductList from './pages/ProductList';
-import Register from './pages/Register';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ScrollToTop from './ScrollToTop';
-
 function App() {
-  return (
-    <Router>
-      <ScrollToTop>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/product" element={<ProductInfo />} />
-        </Routes>
-      </ScrollToTop>
-    </Router>
-  );
+  return <div className="App">hello world</div>;
 }
 
 export default App;
